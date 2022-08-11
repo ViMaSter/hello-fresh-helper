2a3a03126f51e1742fabcabaf0f3edf39ebda810
Vincent Mahnke <vincent.mahnke@proximity.de>
2022-08-11 11:45:20 +0200

    Added support for already selected meals

--- extensions/chrome/js/helloFreshExtension.js
+++ extensions/chrome/js/helloFreshExtension.js
@@ -1,12 +1,21 @@
 {
 	{
-			let checkForReady = setInterval(() => {
+		let checkForReadyUnselected = setInterval(() => {
 			if (document.querySelector("[data-test-id=lazy-load-courses]") && !document.querySelector("[data-test-id=lazy-load-courses] [data-test-id=loading-placeholder]"))
 			{
-				clearInterval(checkForReady);
-				init();
+				clearInterval(checkForReadyUnselected);
+				initUnselected();
 			}
 		}, 500);
+		
+		let checkForReadySelected = setInterval(() => {
+			if (Array.from(document.querySelectorAll("[data-test-id=selected-section-wrapper] [data-test-wrapper-id=recipe-component]")).length > 0)
+			{
+				clearInterval(checkForReadySelected);
+				initSelected();
+			}
+		}, 500);
+
 	}
 
 	let authorizationHeader = null;
@@ -88,7 +97,7 @@
 		}
 	}
 
-	const init = async () => {
+	const initUnselected = async () => {
 		const nextData = JSON.parse(document.querySelector("#__NEXT_DATA__").innerHTML);
 		authorizationHeader = `${nextData.props.pageProps.ssrPayload.serverAuth.token_type} ${nextData.props.pageProps.ssrPayload.serverAuth.access_token}`;
 		mealsThisWeek = Array.from(document.querySelectorAll("[data-test-id=filtered-courses] [data-test-id=course-card]")).map(elementToRecipeID).map(toPromiseOfRecipeContent);
@@ -103,4 +112,19 @@
 		}
 	};
 
+	const initSelected = async () => {
+		const nextData = JSON.parse(document.querySelector("#__NEXT_DATA__").innerHTML);
+		debugger;
+		authorizationHeader = `${nextData.props.pageProps.ssrPayload.serverAuth.token_type} ${nextData.props.pageProps.ssrPayload.serverAuth.access_token}`;
+		mealsThisWeek = Array.from(document.querySelectorAll("[data-test-id=selected-section-wrapper] [data-test-wrapper-id=recipe-component]")).map(elem=>elem.parentElement).map(elementToRecipeID).map(toPromiseOfRecipeContent);
+		if (mealsThisWeek.length < 0)
+		{
+			throw new Error("did you scroll to the recipe list?");
+		}
+		mealsThisWeek = await Promise.all(mealsThisWeek);
+		for (entry of mealsThisWeek)
+		{
+			updateElement(entry);
+		}
+	};
 }
